ec60bf6453cc02c4eeb5d41e5d948748e9aa18a2
chintan ghevariya <73758067+chintanghevariya@users.noreply.github.com>
2023-05-04 20:32:41 -0400

    commit through secondary branch

--- Student/src/Student.java
+++ Student/src/Student.java
@@ -1,10 +1,43 @@
 public class Student {
     private String name, email, userName, passWord;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
     public Student(String name, String email, String userName, String passWord) {
         this.name = name;
         this.email = email;
         this.userName = userName;
         this.passWord = passWord;
+
     }
 }
